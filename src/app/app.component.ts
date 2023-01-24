6adfb8d58f3da50e5830029ad8a78c5cf66ba9c4
N4THB3N <ncastaneda@basiscons.com>
2023-01-24 17:31:15 -0600

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { callJsFun } from '../app/scrollfunction.js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Marios Mechanic Shop';
+  ngOnInit():void{
+    callJsFun()
+  }
+}
